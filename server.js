17c089ac3579061ebaf1dc1dc839e5e1197f4c22
mkawsar <kawsar.diu888@gmail.com>
2018-07-02 14:23:12 +0600

    jwt token created

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
 
 // create express app
 const app = express();
@@ -29,6 +30,47 @@ mongoose.connect(dbConfig.url)
 app.get('/', (req, res) => {
     res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
 });
+
+// jwt authentication login
+app.post('/api/login', (req, res) => {
+    // mock user
+    const user = {
+        id: 1,
+        email: 'admin@example.com',
+        username: 'admin'
+    }
+
+    jwt.sign({user}, 'secretkey',  { expiresIn: '30s' }, (err, token) => {
+        res.json({
+            token
+        })
+    })
+});
+
+
+// FORMAT OF TOKEN
+// Authorization: Bearer <access_token>
+
+// Verify Token
+function verifyToken(req, res, next) {
+    // Get auth header value
+    const bearerHeader = req.headers['authorization'];
+    // Check if bearer is undefined
+    if(typeof bearerHeader !== 'undefined') {
+      // Split at the space
+      const bearer = bearerHeader.split(' ');
+      // Get token from array
+      const bearerToken = bearer[1];
+      // Set the token
+      req.token = bearerToken;
+      // Next middleware
+      next();
+    } else {
+      // Forbidden
+      res.sendStatus(403);
+    }
+  
+  }
 
 // route define
 require('./app/routes/note.routes')(app);
