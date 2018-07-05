52c6f2b864c1af35f1d9baa88a674d7f97ccbfbd
mkawsar <kawsar.diu888@gmail.com>
2018-07-05 12:37:06 +0600

    modifed config user route

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -1,5 +1,8 @@
 const auth = require('../controllers/auth/auth.controller');
 module.exports = (app) => {
+    // user login route
     app.post('/login', auth.login);
+
+    // create new user account
     app.post('/sign-up', auth.create);
 };
--- app/routes/note.routes.js
+++ app/routes/note.routes.js
@@ -3,17 +3,17 @@ module.exports = (app) => {
     const checkAuth = require('../middleware/auth.check');
 
     // create new note
-    app.post('/notes', notes.create);
+    app.post('/notes', checkAuth, notes.create);
 
     // retrieve all Notes
     app.get('/notes', checkAuth, notes.findAll);
 
     // retrieve a single Note with noteId
-    app.get('/notes/:noteId', notes.findOne);
+    app.get('/notes/:noteId', checkAuth, notes.findOne);
 
     // update a note with noteId
-    app.put('/notes/:noteId', notes.update);
+    app.put('/notes/:noteId', checkAuth, notes.update);
 
     // delete a Note with noteId
-    app.delete('/notes/:noteId', notes.delete);
+    app.delete('/notes/:noteId', checkAuth, notes.delete);
 };
